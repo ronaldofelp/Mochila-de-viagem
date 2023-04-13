fda265815ad14ba8ae0ba50b8f38e47bf9fae58b
ronaldofelp <ronaldoamorim144@gmaail.com>
2023-04-13 08:03:53 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const form = document.querySelector("#novoItem")
+
+const lista = document.querySelector("#lista")
+
+const itens = []
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    const nome = evento.target.elements['nome']
+
+    const quantidade = evento.target.elements['quantidade']
+
+    criaElemento(nome.value, quantidade.value)
+
+    nome.value = ""
+    quantidade.value = ""
+})
+
+
+function criaElemento(nome, quantidade) {
+
+
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = quantidade
+
+    novoItem.appendChild(numeroItem)
+    novoItem.innerHTML += nome
+
+    lista.appendChild(novoItem)
+
+    const itemAtual = {
+        "nome": nome,
+        "quantidade": quantidade
+    }
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("item", JSON.stringify(itens))
+
+
+
+}
